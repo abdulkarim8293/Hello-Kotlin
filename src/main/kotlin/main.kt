4e5f1569ae7b15b668d3f82c30a64f67f93a8a71
unknown <abdulkarim8293@gmail.com>
2021-07-11 11:00:59 +0600

    basic syntex

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,24 @@
+
+/*
+* Created by Abdul Karim
+* basic syntex
+* */
+
 fun main(args: Array<String>) {
 
     val myName = "Abdul Karim"
+    Message(myName).display()
 
-    println("Hello!")
-    println("My name is $myName")
+}
+
+class Message(private val text: String) {
+
+    init {
+        println("Hello!")
+    }
+
+    fun display() {
+        println("My name is $text")
+    }
 
 }
